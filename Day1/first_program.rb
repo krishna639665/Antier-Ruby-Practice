2b2277ef14b2fcef3d7805975d6a3ab05fcde89b
Krishna <kumarkrishna36592@gmail.com>
2022-04-06 11:07:06 +0530

    use case statement

--- Day1/first_program.rb
+++ Day1/first_program.rb
@@ -1,2 +1,17 @@
 # frozen_string_literal: true
-puts "\n\t#{(2+5)*2}"
+print"Enter your day: "
+day = gets.chomp
+case day
+when "Tuesday"
+  puts 'Wear Red or Orange'
+when "Wednesday"
+  puts 'Wear Green'
+when "Thursday"
+  puts 'Wear Yellow'
+when "Friday"
+  puts 'Wear White'
+when "Saturday"
+  puts 'Wear Black'
+else
+  puts "Wear Any color"
+end
